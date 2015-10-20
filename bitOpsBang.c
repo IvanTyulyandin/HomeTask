9ec566297992e463118ca6b43b6492eceb8a83cb
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2015-10-21 00:57:38 +0400

    Update bitOpsBang.c

--- bitOpsBang.c
+++ bitOpsBang.c
@@ -2,7 +2,7 @@ int bang(int x)
 {
     int frt = 0, scd = 0, msk = 1;
     msk = msk << 31;
-    frt = (frt & msk) >> 31;
+    frt = x >> 31; 
     scd = (~x + 1) >> 31;
-    return ~(frt ^ scd) & 1; // for 0 or 1 doing &1
+    return ~(frt ^ scd) & 1; 
 }
