29e92df88777027c62c6952a6b0d6e782aa53693
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2015-12-03 21:44:09 +0400

    Update bitOpsAddOk.c

--- bitOpsAddOk.c
+++ bitOpsAddOk.c
@@ -1,9 +1,4 @@
-int addOk(int x, int y)
+int addOk (int x, int y)
 {
-    int sgnX = 0, sgnY = 0, sgnRes = 0, sgnH = 0;
-    sgnX = (x >> 31) & 1;
-    sgnY = (y >> 31) & 1;
-    sgnRes = ((x + y) >> 31) & 1;
-    sgnH = !(sgnX) | !(sgnY);
-    return ((sgnX ^ sgnY) & sgnRes) | sgnH;
+	return !(~(x >> 31 ^ y >> 31) & (x >> 31 ^ ((x + y) >> 31)));
 }
