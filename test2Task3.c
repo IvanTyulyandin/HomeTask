a00f9d648338b65450f51ff809d978362f1e1cf5
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2015-12-20 21:24:38 +0400

    Update test2Task3.c

--- test2Task3.c
+++ test2Task3.c
@@ -1,41 +1,56 @@
-void searchSlash()
+#include <stdio.h>
+#include <string.h>
+
+const int maxLen = 255;
+
+void searchDoubleSlash(FILE *f)
 {
+    char *str;
+    str = (char*)malloc(maxLen * sizeof(char));
+    if (str != NULL)
+    {
+        char prev;
+        int i = 0;
+        int wereTwoSlash;
+        while (fgets(str, maxLen, f) != NULL)
+        {
+            prev = str[0];
+            if (prev != '\n')
+            {
+                wereTwoSlash = 0;
+                for (i = 1; str[i] != 0; i++)
+                {
+                    if (wereTwoSlash == 1)
+                    {
+                        printf("%c", str[i]);
+                    }
+                    if ((str[i] == '/') && (prev == '/'))
+                    {
+                        wereTwoSlash = 1;
+                        printf("//");
+                    }
+                    prev = str[i];
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("Not enough memory!");
+        return;
+    }
+}
+
+int main()
+{
+
     FILE *f = fopen("input.txt", "r");
     if (f == NULL)
     {
         printf("File wasn't opened!");
-        return;
+        exit(1);
     }
-    else
-    {
-        char *str;
-        str = (char*)malloc(maxLen * sizeof(char));
-        if (str != NULL)
-        {
-            char prev;
-            int i = 0;
-            while (fgets(str, maxLen, f) != NULL)
-            {
-                prev = str[0];
-                for (i = 1; str[i] != 0; i++)
-                {
-                    if ((prev == '/') && (prev == str[i]))
-                    {
-                        printf(str);
-                        break;
-                    }
-                    else
-                    {
-                        prev = str[i];
-                    }
-                }
-            }
-        }
-        else
-        {
-            printf("Not enough memory!");
-            return;
-        }
-        fclose(f);
-    }
+    searchDoubleSlash(f);
+    fclose(f);
+    return 0;
 }
