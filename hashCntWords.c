770f54d2d24e0edeb940ee76e569974d2ad846df
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2015-12-24 18:36:58 +0400

    Update hashCntWords.c

--- hashCntWords.c
+++ hashCntWords.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <time.h>
 
 typedef struct node node;
 typedef struct list list;
@@ -29,7 +30,7 @@ struct hashT
 
 int hashConst(char *key, hashT* ht)
 {
-    return 19;
+    return 19 % ht -> size;
 }
 
 int hashSum(char *key, hashT* ht)
@@ -95,7 +96,6 @@ void handleNewElem(char *key, hashT* ht)
 {
     int (*pH)(char*, hashT*) = ht -> hashFunc;
     int offset = (*pH)(key, ht);
-    printf("%d ", offset);
     list *curList = ht -> entry[offset];
     node *cur = curList -> head;
     while ((cur != NULL) && (strcmp(cur -> key, key) != 0))
@@ -181,29 +181,107 @@ int cntAllElems(hashT *ht)
     return res;
 }
 
-int main()
+int cntNotNullChains(hashT* ht)
 {
-    const int sizeConst = 1;
-    const int sizeSum = 5000;
-    const int sizeRot13 = 2147483648; // 2^31;
+    list *curList;
+    int i, res = 0;
+    for (i = 0; i < ht -> size; i++)
+    {
+        curList = ht -> entry[i];
+        if (curList -> head != NULL)
+        {
+            res++;
+        }
+    }
+    return res;
+}
 
-    FILE *f = fopen("Oscar Wilde_The Picture of Dorian Gray.txt", "r");
+float cntMediumLen(hashT *ht)
+{
+    list *curList;
+    int i;
+    float res;
+    for (i = 0; i < ht -> size; i++)
+    {
+        curList = ht -> entry[i];
+        node *cur = curList -> head;
+        while (cur != NULL)
+        {
+            res += 1;
+            cur = cur -> next;
+        }
+    }
+    printf("", res, ht -> size);
+    res = res / (ht -> size);
+    return res;
+}
+
+int cntMinNotNull(hashT *ht)
+{
+    list *curList;
+    int i, minLen = 100000, curLen;
+    node *cur;
+    curList = ht -> entry[0];
+    cur = curList -> head;
+    for (i = 0; i < ht -> size; i++)
+    {
+        curList = ht -> entry[i];
+        curLen = 0;
+        cur = curList -> head;
+        while (cur != NULL)
+        {
+            curLen++;
+            cur = cur -> next;
+        }
+        if ((curLen < minLen) && (curLen != 0))
+        {
+            minLen = curLen;
+        }
+    }
+    if (minLen != 0)
+    {
+        return minLen;
+    }
+    else
+    {
+        printf("Table is empty!\n");
+        return 0;
+    }
+}
+
+int cntMaxLen(hashT *ht)
+{
+    list *curList;
+    int i, curLen, maxLen = 0;
+    node *cur;
+    for (i = 0; i < (ht -> size); i++)
+    {
+        curList = ht -> entry[i];
+        cur = curList -> head;
+        curLen = 0;
+        while (cur != NULL)
+        {
+            curLen++;
+            cur = cur -> next;
+        }
+        if (curLen > maxLen)
+        {
+            maxLen = curLen;
+        }
+    }
+    return maxLen;
+}
+
+void useHt(int(*fp)(char* , hashT*), int size, char *s)
+{
+    FILE *f = fopen("input.txt", "r");
     if (f == NULL)
     {
         printf("File wasn't opened!");
         exit(1);
     }
-    int (*fp)(char*, hashT*);
-    fp = hashRot13;
     hashT *ht;
-    ht = createHashT(fp, sizeRot13);
-    char *s;
-    s = (char*)malloc(sizeof(char) * maxLenStr);
-    if (s == NULL)
-    {
-        printf("Not enough memory!");
-        exit(1);
-    }
+    ht = createHashT(fp, size);
     int c, i, curState = 0, amount = 0;
     while ((c = fgetc(f)) != EOF)
     {
@@ -243,6 +321,56 @@ int main()
         handleNewElem(s, ht);
     }
     fclose(f);
-    printf("%d", cntAllElems(ht));
+    printf("    Different words %d\n", cntAllElems(ht));
+    printf("    Max length of chain %d\n", cntMaxLen(ht));
+    printf("    Medium length %.5f\n", cntMediumLen(ht));
+    printf("    Length of minimum not null chain %d\n", cntMinNotNull(ht));
+    printf("    Used baskets %d\n", cntNotNullChains(ht));
+    delTable(&ht);
+}
+
+int main()
+{
+    const int sizeConst = 1;
+    const int sizeSum = 5000;
+    const int sizeRot13 = 100000;
+    int allSize[3] = {sizeConst, sizeSum, sizeRot13};
+    int hashArr[3] = {hashConst, hashSum, hashRot13};
+    int sizeArr = 3;
+    char *s;
+    s = (char*)malloc(sizeof(char) * maxLenStr);
+    if (s == NULL)
+    {
+        printf("Not enough memory!");
+        exit(1);
+    }
+    int i;
+    clock_t start, delta;
+    for (i = 0; i < sizeArr; i++)
+    {
+        switch (i)
+        {
+            case 0:
+                {
+                   printf("hashConst\n");
+                   break;
+                }
+            case 1:
+                {
+                    printf("hashSum\n");
+                    break;
+                }
+            case 2:
+                {
+                    printf("hashRot13\n");
+                    break;
+                }
+        }
+        start = clock();
+        useHt(hashArr[i], allSize[i], s);
+        delta = clock() - start;
+        printf("    %.4f\n\n", (double)delta / CLOCKS_PER_SEC);
+    }
+    free(s);
     return 0;
 }
