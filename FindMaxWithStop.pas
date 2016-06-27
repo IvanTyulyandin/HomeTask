0ddb78548adcbcf6b43ab52a8d72117c59980126
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2016-06-27 13:29:01 +0300

    Update FindMaxWithStop.pas

--- FindMaxWithStop.pas
+++ FindMaxWithStop.pas
@@ -1,19 +1,21 @@
 program task;
 var teknum, maxNum, stopNum : integer;
+  f : text;
 {tekNum - текущее считываемое число}
 {maxNum - максимальное число}
 {stopNum - число остановки}
 begin
-  writeln('Введите символ остановки');
-  readln(stopNum);
-  maxNum := -2147483648; {максимальное отрицательное число в ABC паскале}
+  assign(f, 'input.txt'); {если нужно, то имя файла ввода можно поменять}
+  reset(f);
+  readln(f, stopNum);
+  maxNum := -2147483648; {максимальное отрицательное число в ABC паскале -2^31}
   tekNum := stopNum - 1; {делаем так, чтоб точно зайти в цикл хотя бы один раз}
   while (tekNum <> StopNum) do
     begin
-      writeln('Введите очередной символ');
-      readln(tekNum);
+      readln(f, tekNum);
       if (tekNum > maxNum)
       then maxNum := tekNum;
     end;
+  close(f);
   writeln('Максимальное число = ', maxNum);
 end.
